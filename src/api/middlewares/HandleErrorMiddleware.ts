a6cb74d5bb9461d2cf402aefe2a3d55958f54447
marafoo <pttx00@protonmail.com>
2022-03-09 11:45:07 -0300

    Adding jsdoc

--- src/api/middlewares/HandleErrorMiddleware.ts
+++ src/api/middlewares/HandleErrorMiddleware.ts
@@ -1,6 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import { CustomError } from '../errors/CustomError';
 
+/**
+ * @description Middleware para evitar que informaçẽos sensíveis como stack
+ * trace seja retornada no resposta do usuário
+ * @param error
+ * @param request
+ * @param response
+ * @param next
+ */
+
 export function HandleErrorMiddleware(
   error: TypeError | CustomError,
   request: Request,
